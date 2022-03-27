f1c0d0eaaa443c5b97eb8a11d06c9911662b3ad6
Matteo Beniamino <beniamino@tautologica.org>
2022-03-27 14:56:48 +0200

    Code cleaning and refactoring

--- bopen.c
+++ bopen.c
@@ -71,6 +71,7 @@ typedef struct Context {
     char* filepath;
     GFile* file;
     GtkWindow* main_window;
+    GtkWidget *chooser;
 } Context;
 
 void app_chooser_response_cb(GtkDialog* self, gint response_id, gpointer user_data) {
@@ -78,10 +79,12 @@ void app_chooser_response_cb(GtkDialog* self, gint response_id, gpointer user_da
     GError *error = NULL;
     if (response_id == GTK_RESPONSE_OK) {
         GAppInfo* app_info = gtk_app_chooser_get_app_info(
-                (GtkAppChooser*)gtk_app_chooser_dialog_get_widget((GtkAppChooserDialog*)self));
+                GTK_APP_CHOOSER(gtk_app_chooser_dialog_get_widget(GTK_APP_CHOOSER_DIALOG(self))));
         GList *files = NULL;
         files = g_list_append(files, context->file);
         g_app_info_launch(app_info, files, NULL, &error);
+        if (error) g_error_free(error);
+        g_list_free(files);
     }
     gtk_window_destroy(context->main_window);
 }
@@ -91,47 +94,72 @@ activate (GtkApplication *app,
           gpointer        user_data) {
           Context* context = (Context*)user_data;
 
-          GtkWindow *window;
-          window = (GtkWindow*)gtk_application_window_new(app);
-          GtkWidget* chooser = gtk_app_chooser_dialog_new(window, GTK_DIALOG_MODAL, context->file);
-          context->main_window = window;
+          context->main_window = GTK_WINDOW(gtk_application_window_new(app));
+          context->chooser = gtk_app_chooser_dialog_new(context->main_window, GTK_DIALOG_MODAL,
+                                                        context->file);
           context->filepath = (char*)user_data;
-          g_signal_connect(chooser,
+          g_signal_connect(context->chooser,
                            "response",
                            G_CALLBACK(app_chooser_response_cb),
                            context);
-          // To make the chooser open as a floating window for some reason we have to
-          // show the parent window. We can hide it just after.
-          gtk_widget_show((GtkWidget*)window);
-          gtk_widget_show(chooser);
-          gtk_widget_hide((GtkWidget*)window);
+          gtk_window_set_title(GTK_WINDOW(context->chooser), "BOpen");
+          gtk_window_present(context->main_window);
+          gtk_widget_hide(GTK_WIDGET(context->main_window));
+          gtk_widget_show(context->chooser);
 }
 // ============================================================================
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        exit(0);
-    }
-    const char* path = argv[1];
+typedef struct BOpen {
+    const char* file_path;
+    char* file_ext;
+    FILE* config_f;
+} BOpen;
+
+void init(BOpen* bopen, const char* file_path) {
+    bopen->file_path = file_path;
+    bopen->file_ext = NULL;
+    FILE* config_f = 0;
+
+    // Check if file exists
     struct stat statbuf;
-    if (stat(path, &statbuf) != 0) {
-        fprintf(stderr, "Couldn't stat %s. Exiting.", path);
+    if (stat(bopen->file_path, &statbuf) != 0) {
+        fprintf(stderr, "Couldn't stat %s. Exiting.", bopen->file_path);
         exit(2);
     }
-    char* ext = extension(path);
+    // Retrieve and set the file extension
+    bopen->file_ext = extension(bopen->file_path);
+}
+
+void close_config_file(BOpen* bopen) {
+    if(bopen->config_f) fclose(bopen->config_f);
+    bopen->config_f = 0;
+}
+
+void deinit(BOpen* bopen) {
+    free(bopen->file_ext);
+    bopen->file_ext = NULL;
+    close_config_file(bopen);
+}
+
+void open_config_file(BOpen* bopen) {
+    // Open the configuration file
     const char* CONFIG_REL_PATH = "/.config/bopen.cfg";
     struct passwd *pw = getpwuid(getuid());
     const char *homedir = pw->pw_dir;
     char* cfgpath = malloc(strlen(homedir)+strlen(CONFIG_REL_PATH)+1);
     strcpy(cfgpath, homedir);
     strcat(cfgpath, CONFIG_REL_PATH);
-    FILE* f = fopen(cfgpath, "rb");
-    if (!f) {
+    bopen->config_f = fopen(cfgpath, "rb");
+    if (!bopen->config_f) {
         fprintf(stderr, "Error: couldn't read configuration file %s\n", cfgpath);
-        free(cfgpath);
         exit(2);
     }
+    free(cfgpath);
+}
+
+void launch_associated_app(BOpen* bopen) {
+    open_config_file(bopen);
+    FILE* f = bopen->config_f;
     int eof = 0;
     while(!eof) {
         int ch = getc(f);
@@ -150,11 +178,15 @@ int main(int argc, char* argv[]) {
                 break;
             default:
                 if(!read_to(f, '=', &extension)) break;
-                if(!read_to(f, ';', &executable)) break;
-                printf("%s == %s?\n", extension, ext);
-                if (strcmp(extension, ext) == 0) {
-                    execl(executable, executable, path, (char*)NULL);
-                    fprintf(stderr, "Error executing %s %s.\n", executable, path);
+                if(!read_to(f, ';', &executable)) {
+                    free(extension);
+                    break;
+                }
+                if (strcasecmp(extension, bopen->file_ext) == 0) {
+                    execl(executable, executable, bopen->file_path, (char*)NULL);
+                    fprintf(stderr, "Error executing %s %s.\n", executable, bopen->file_path);
+                    free(extension);
+                    free(executable);
                     break;
                 }
                 free(extension);
@@ -162,11 +194,22 @@ int main(int argc, char* argv[]) {
                 next_line(f);
         }
     }
-    fclose(f);
-    free(cfgpath);
+    close_config_file(bopen);
+}
+
+int main(int argc, char* argv[]) {
+    // No args: print help
+    if (argc < 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        exit(0);
+    }
+
+    BOpen bopen;
+    init(&bopen, argv[1]);
+    launch_associated_app(&bopen);
 
     // No association found... use gtk4 dialog to let the user choose a proper application
-    GFile* gf = g_file_new_for_path(path);
+    GFile* gf = g_file_new_for_path(bopen.file_path);
     if(gf) {
         int status;
         GtkApplication *app;
@@ -175,7 +218,11 @@ int main(int argc, char* argv[]) {
         app = gtk_application_new ("org.tautologica.bopen", G_APPLICATION_FLAGS_NONE);
         g_signal_connect(app, "activate", G_CALLBACK (activate), &context);
         status = g_application_run(G_APPLICATION (app), 0, NULL);
+        g_object_unref(gf);
         g_object_unref(app);
     }
+
+    deinit(&bopen);
+
     return 0;
 }
--- make
+++ make
@@ -1,4 +1,4 @@
 #!/bin/sh
 LINKER_FLAGS=$(pkg-config --libs gtk4)
 COMPILER_FLAGS=$(pkg-config --cflags gtk4)
-gcc -g bopen.c ${LINKER_FLAGS} ${COMPILER_FLAGS} -o bopen
+gcc bopen.c -O2 ${LINKER_FLAGS} ${COMPILER_FLAGS} $@ -o bopen
