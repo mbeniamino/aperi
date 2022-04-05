5f0633c43087cbaa43ff2013ea2b4a0c52d877f9
Matteo Beniamino <beniamino@tautologica.org>
2022-04-05 20:10:18 +0200

    Moved gtk4 opener to a separate executable

--- aperi.c
+++ aperi.c
@@ -7,9 +7,6 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
-#ifdef HAVE_GTK4
-#include <gtk/gtk.h>
-#endif
 
 int read_to(FILE* f, const char sep) {
     while(1) {
@@ -113,51 +110,6 @@ int rule_id(const char* path, char** ext) {
     for(idx = 0; (*ext)[idx]; ++idx) (*ext)[idx] = tolower((*ext)[idx]);
     return !exists;
 }
-
-#ifdef HAVE_GTK4
-// Code used for the Gtk dialog ===============================================
-typedef struct Context {
-    char* filepath;
-    GFile* file;
-    GtkWindow* main_window;
-    GtkWidget *chooser;
-} Context;
-
-void app_chooser_response_cb(GtkDialog* self, gint response_id, gpointer user_data) {
-    Context *context = (Context*)user_data;
-    GError *error = NULL;
-    if (response_id == GTK_RESPONSE_OK) {
-        GAppInfo* app_info = gtk_app_chooser_get_app_info(
-                GTK_APP_CHOOSER(gtk_app_chooser_dialog_get_widget(GTK_APP_CHOOSER_DIALOG(self))));
-        GList *files = NULL;
-        files = g_list_append(files, context->file);
-        g_app_info_launch(app_info, files, NULL, &error);
-        if (error) g_error_free(error);
-        g_list_free(files);
-    }
-    gtk_window_destroy(context->main_window);
-}
-
-static void
-activate (GtkApplication *app,
-          gpointer        user_data) {
-          Context* context = (Context*)user_data;
-
-          context->main_window = GTK_WINDOW(gtk_application_window_new(app));
-          context->chooser = gtk_app_chooser_dialog_new(context->main_window, GTK_DIALOG_MODAL,
-                                                        context->file);
-          context->filepath = (char*)user_data;
-          g_signal_connect(context->chooser,
-                           "response",
-                           G_CALLBACK(app_chooser_response_cb),
-                           context);
-          gtk_window_set_title(GTK_WINDOW(context->chooser), "Aperi");
-          gtk_window_present(context->main_window);
-          gtk_widget_hide(GTK_WIDGET(context->main_window));
-          gtk_widget_show(context->chooser);
-}
-// ============================================================================
-#endif
 
 typedef struct Aperi {
     const char* file_path;
@@ -301,26 +253,7 @@ int main(int argc, char* argv[]) {
     launch_associated_app(&aperi);
 
     int status = 0;
-#ifdef HAVE_GTK4
-    // No association found... use gtk4 dialog to let the user choose a proper application
-    GFile* gf = g_file_new_for_path(aperi.file_path);
-    if(gf) {
-        GtkApplication *app;
-        Context context;
-        context.file = gf;
-        app = gtk_application_new ("org.tautologica.aperi", G_APPLICATION_FLAGS_NONE);
-        g_signal_connect(app, "activate", G_CALLBACK (activate), &context);
-        status = g_application_run(G_APPLICATION (app), 0, NULL);
-        g_object_unref(gf);
-        g_object_unref(app);
-    }
-#endif
     deinit(&aperi);
-
-#ifndef HAVE_GTK4
-    fprintf(stderr, "No applications configured to handle %s\n", argv[1]);
-    status = 1;
-#endif
 
     return status;
 }
