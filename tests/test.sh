9073e069d82692576a99b84992d3463849e81959
Matteo Beniamino <beniamino@tautologica.org>
2025-05-23 18:50:55 +0200

    Added simple test script

---
+++ tests/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+BASEDIR=$(dirname "$0")
+export XDG_CONFIG_HOME="$BASEDIR/config"
+tmpfile=$(mktemp /tmp/aperi_tests.XXXXXX)
+exec 3>"$tmpfile"
+exec 4<"$tmpfile"
+rm "$tmpfile"
+for f in files/*; do echo "===$f==="; ../build/aperi "$f"; echo; done |\
+    sed "s|$(realpath ../tests/files)/||" >&3
+diff --from-file=- reference.out <&4
