487669f4ed2426189a3ee2ac71c464591d18b892
Matteo Beniamino <beniamino@tautologica.org>
2025-07-04 16:54:34 +0200

    Added tests for https://codeberg.org/mbeniamino/aperi/issues/1

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,6 @@ tmpfile=$(mktemp /tmp/aperi_tests.XXXXXX)
 exec 3>"$tmpfile"
 exec 4<"$tmpfile"
 rm "$tmpfile"
-for f in files/*; do echo "===$f==="; ../build/aperi "$f"; echo; done |\
+for f in files/* http://test; do echo "===$f==="; ../build/aperi "$f"; echo; done |\
     sed "s|$(realpath ../tests/files)/||" >&3
 diff --from-file=- reference.out <&4
